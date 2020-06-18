6ecd7475746c9f025b47ce5f9b7537adf7995364
henrique <hbaetaleite@gmail.com>
2020-06-18 12:11:59 -0300

    tentando fazer deploy no github

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="main">
         <h1>Conversor de Moedas</h1>
         <div className="container">
-          <Conversor2 moedaA="AUD" moedaB="BRL"/>
           <Conversor2 moedaA="USD" moedaB="BRL"/>
           <Conversor2 moedaA="EUR" moedaB="BRL"/>
+          <Conversor2 moedaA="BRL" moedaB="USD" />
           <Conversor2 moedaA="BRL" moedaB="EUR" />
         </div>
         <p>Desenvolvido por <a href="https://www.linkedin.com/in/henrique-ba%C3%AAta-leite-785a4b15a/">Henrique Baêta Leite</a>&copy;</p>
