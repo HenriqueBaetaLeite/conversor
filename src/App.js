59d4124161ceeb25281ede464178ae7c253cc0e4
henrique <hbaetaleite@gmail.com>
2020-07-24 20:41:13 -0300

    deploy do projeto

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Conversor2 from './components/Conversor2';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="main">
-        <h1>Conversor de Moedas</h1>
-        <div className="container">
-          <Conversor2 moedaA="USD" moedaB="BRL"/>
-          <Conversor2 moedaA="EUR" moedaB="BRL"/>
-          <Conversor2 moedaA="BRL" moedaB="USD" />
-          <Conversor2 moedaA="BRL" moedaB="EUR" />
-        </div>
-        <p>Desenvolvido por <a href="https://www.linkedin.com/in/henrique-ba%C3%AAta-leite-785a4b15a/">Henrique Baêta Leite</a>&copy;</p>
+const App = () => {
+  return (
+    <div className="main">
+      <h1>Conversor de Moedas</h1>
+      <div className="container">
+        <Conversor2 moedaA="USD" moedaB="BRL" />
+        <Conversor2 moedaA="EUR" moedaB="BRL" />
+        <Conversor2 moedaA="BRL" moedaB="USD" />
+        <Conversor2 moedaA="BRL" moedaB="EUR" />
       </div>
-    );
-  }
-}
+      <p>
+        Esse App utiliza a API{' '}
+        <a href="https://www.currencyconverterapi.com/" target="_blank" rel="noopener noreferrer">
+          Free Current Conversor
+        </a>
+        , os valores são atualizados a cada 60 minutos.
+      </p>
+      <p className="subtitle">
+        Legenda: <br />
+        USD - dolar U$ <br />
+        BRL - real R$ <br />
+        EUR - euro &euro; <br />
+      </p>
+      <p>
+        Desenvolvido por{' '}
+        <a
+          href="https://www.linkedin.com/in/henrique-ba%C3%AAta-leite-785a4b15a/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Henrique Baêta Leite
+        </a>
+        &copy; 2020
+      </p>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Conversor2.jsx
+++ src/components/Conversor2.jsx
@@ -6,37 +6,41 @@ export default class Conversor2 extends Component {
     this.state = {
       moedaA_valor: 0,
       moedaB_valor: 0,
-    }
-
-    this.converter = this.converter.bind(this);
-
+    };
   }
 
-  converter() {
-    let de_para = `${this.props.moedaA}_${this.props.moedaB}`;
-    let url = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=ultra&apiKey=dd4a3aceab22b960df05`;
-    
-    fetch(url)
-      .then(res => res.json())
-      .then(json => {
-        const cotacao = json[de_para];
+  converter = async () => {
+    let convertion = `${this.props.moedaA}_${this.props.moedaB}`;
+    let url = `https://free.currconv.com/api/v7/convert?q=${convertion}&compact=ultra&apiKey=dd4a3aceab22b960df05`;
+
+    await fetch(url)
+      .then((res) => res.json())
+      .then((json) => {
+        const cotacao = json[convertion];
         const moedaAtual = this.state.moedaA_valor;
         let moedaB_valor = (moedaAtual * cotacao).toFixed(2).replace('.', ',');
-        this.setState({moedaB_valor})
+        this.setState({ moedaB_valor });
       })
-      .catch(err => console.log('o erro foi: ', err))
-  }
+      .catch((err) => console.log('o erro foi: ', err));
+  };
 
   render() {
     return (
       <div className="conversor">
-        <h4>{this.props.moedaA} para {this.props.moedaB}</h4>
-        <input type="text" size="10" placeholder="valor $" onChange={(event) => {this.setState({moedaA_valor: event.target.value})}}/>
+        <h4>
+          {this.props.moedaA} para {this.props.moedaB}
+        </h4>
+        <input
+          type="text"
+          size="10"
+          placeholder="digite valor..."
+          onChange={(event) => {
+            this.setState({ moedaA_valor: event.target.value });
+          }}
+        />
         <input className="button" type="button" value="converter" onClick={this.converter} />
         <h4>Valor convertido:</h4>
         <h4>${this.state.moedaB_valor}</h4>
-        
-
       </div>
     );
   }
